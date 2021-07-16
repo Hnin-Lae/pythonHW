ba311220f6c75a39d846e06f361c593ab7cf3098
Hnin Lae Cho <c19052@sangi.jp>
2021-07-16 16:40:06 +0900

    first commit

---
+++ loops.py
@@ -0,0 +1,27 @@
+# names=['hank','hnin','wankexing','arrshwi'];
+
+# for name in  names:
+#     #print(name)
+#     if name=='hnin' :
+#         print(f'{name} is instructor')
+#         break
+#     else:
+#         print(f'{name} is student')
+    
+# fruits=['apple','orange','watermelon']
+
+# for fruit in fruits:
+    # print(f'{fruit} is fruit')
+
+#while loop
+
+# while 0<10 : #it never stop cause it is always true
+#     print('Hi')
+
+num=0
+while num < 10:
+    if num > 5 :
+        break
+    if num % 2 == 0 :
+        print(f'{num} ')
+    num +=1;
